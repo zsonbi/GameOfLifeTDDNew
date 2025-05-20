342d18d837c15ee7bec7ebb2c063f80e085ba0c5
zsonbi <zsomborder@gmail.com>
2025-05-20 16:45:55 +0200

    Add project files.

---
+++ Tests/Tests.cs
@@ -0,0 +1,59 @@
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void ReadInFileTest()
+        {
+
+        }
+
+        [Fact]
+        public void ParseRLEFileTest()
+        {
+
+        }
+
+        [Fact]
+        public void ASingleGenerationTest()
+        {
+
+        }
+
+        [Fact]
+        public void AloneCellInteractionTest()
+        {
+
+        }
+
+        [Fact]
+        public void OneCellNearbyInteractionTest()
+        {
+
+        }
+
+        [Fact]
+        public void TwoNearbyCellsInteractionTest()
+        {
+
+        }
+
+        [Fact]
+        public void ThreeNeabyCellsInteraction()
+        {
+
+        }
+
+
+        [Theory]
+        [InlineData(2)]
+        public void MultipleGenerationsTest(int generationCount)
+        {
+
+        }
+
+
+
+
+    }
+}
